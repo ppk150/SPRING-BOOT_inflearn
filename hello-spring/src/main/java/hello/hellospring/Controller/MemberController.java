287deba70f82da5995a8ab01e5c6fa851de0d805
ppk150 <vytpdnd11@naver.com>
2023-03-07 21:45:52 +0900

    [20230307] 스프링 웹 개발 기초 회원 웹 기능 - 홈 화면 추가 , 회원 웹 기능 - 등록 ,회원 웹 기능 - 조회

--- hello-spring/src/main/java/hello/hellospring/Controller/MemberController.java
+++ hello-spring/src/main/java/hello/hellospring/Controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.Controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // 컨트롤러 어노테이션 붙여서 스프링 컨테이너 안의 스프링 빈에 넣는다.
 public class MemberController {
@@ -19,5 +25,30 @@ public class MemberController {
     //생성자에 맴버 서비스를 넣어준다 -> 이를 생성자 주입이라고 한다.
 
     // 오토 와이어는 스프링 컨테이너에 등록이 되어 있는 상태에만 동작 가능
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member : " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+
+    }
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members= memberService.findMembers();
+        model.addAttribute("members",members); // 맴버 등록된거 리스트에 다 담는거
+        return "members/memberList";
+    }
 
 }
